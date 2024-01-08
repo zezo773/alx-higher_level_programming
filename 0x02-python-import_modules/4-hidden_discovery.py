16a5b66e11041f544621c4c1671e5105f54b31c2
zezo773 <zezo77328@gmail.com>
2024-01-08 11:08:53 -0800

    ziad

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-import hidden_4
-
 if __name__ == "__main__":
-    for content in dir(hidden_4):
-        if content[:2] != "__":
-            print("{:s}".format(content))#!/usr/bin/python3
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
 
-import hidden_4
-
-if __name__ == "__main__":
-    for content in dir(hidden_4):
-        if content[:2] != "__":
-            print("{:s}".format(content))
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
