54d7b8693002588bdbe3b9311223135bdcf4c3b7
zezo773 <zezo77328@gmail.com>
2024-03-16 06:22:53 -0700

    dsa

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,3 @@
 -- lists all records  in the table of the database
-SELECT score name FROM second_table
-WHERE score >= 10 ORDER BY score DESC ;
+SELECT score, name FROM second_table WHERE score >= 10  ORDER BY score DESC;
 
