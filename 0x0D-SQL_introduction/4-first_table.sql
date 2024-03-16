cdbed9eb8b8c6ecec2e46c0fff186b4153a5b7b5
zezo773 <zezo77328@gmail.com>
2024-03-16 05:48:23 -0700

    asdgf

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table in the current database
-CREATE TABLE first_table(id INT, name VARCHAR(256)); IF NOT EXIST
+CREATE TABLE IF NOT EXIST first_table(id INT, name VARCHAR(256)); --IF NOT EXIST
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create  a table in the database and add multiples rows
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT); IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
 (id, name, score)
 VALUES
