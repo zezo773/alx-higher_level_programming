b148a5b8c738b8e85545ddb3ab4cb826ff5407a3
zezo773 <zezo77328@gmail.com>
2024-03-16 05:46:17 -0700

    dfa

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- create  a table in the database and add multiples rows
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT); IF NOT EXISTS
+INSERT INTO second_table
+(id, name, score)
+VALUES
+(1,  “John”, 10),
+(2,  “Alex”, 3),
+(3, “Bob”, 14),
+(4, “George”, 8);
