16aff84d25c69f3bf3ff34807ec79f50163090b3
zezo773 <zezo77328@gmail.com>
2024-04-13 11:14:33 -0700

    dg

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const len = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let flen = len.length;
+const flen = len.length;
 let i;
 
-for (i = 0; i < flen; i++)
-{
-	console.log(len[i]);
+for (i = 0; i < flen; i++) {
+  console.log(len[i]);
 }
