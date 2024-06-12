f678386e8f0c97e5853ca3314f747afc9792e3a1
zezo773 <zezo77328@gmail.com>
2024-06-12 06:31:33 -0700

    asdf

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-import sys
-
-length = len(sys.argv)
-summ = 0
-if __name__ == "__main__":
-    if length == 1:
-        summ = 0
-    else:
-        for i in range(1, length):
-            summ += int(sys.argv[i])
-    print("{:d}".format(summ))
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1: ]
+   ## if len(args) < 1:
+     ##   print("0")
+    print(sum(map(int, args)))
