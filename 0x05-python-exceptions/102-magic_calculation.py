112e411b86533c6bfee91e13548425660d1f933f
zezo773 <zezo77328@gmail.com>
2024-07-04 03:46:20 -0700

    asdf

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 0
+    res = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
-            result = b + a
+            res += a ** b / i
+        except Exception:
+            res = b + a
             break
-    return (result)
+    return res
