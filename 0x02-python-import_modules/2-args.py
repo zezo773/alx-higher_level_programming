271eebeff7eeb17bc69f976caee7d8bb8b40f184
zezo773 <zezo773@gmail.com>
2025-06-11 14:53:45 +0300

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
 import sys
 
 if __name__ == "__main__":
-    length = len(sys.argv)
 
-    if length == 1:
-        print("{:d} arguments.".format(length - 1))
-    elif length == 2:
-        print("{:d} argument:".format(length - 1))
+    args = sys.argv[1:]
+    count = len(args)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{:d} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{} arguments".format(count))
+
+    for i in range(count):
+        print("{}: {}".format(i + 1, args[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+import sys
 
-if __name__ == '__main__':
-    import sys
-    args = sys.argv[1: ]
-   ## if len(args) < 1:
-     ##   print("0")
-    print(sum(map(int, args)))
+if __name__ == "__main__":
+    sum = 0
+    for arg in sys.argv[1:]:
+        sum += int(arg)
+
+    print(sum)
