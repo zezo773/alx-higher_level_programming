06dc473c92d6e691d06e4a44a809293e168bafed
zezo773 <ziadammar384@gmail.com>
2025-12-08 23:48:52 +0200

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 
+const argv = process.argv.slice(2).map(Number);
+
+if (argv.length < 2) {
+	console.log(0);
+	return;
+}
+let larg = -Infinity;
+let slarg = -Infinity;
+for (let n of argv) {
+	if (n > larg)
+		larg = n;
+}
+for (let n of argv) {
+	if (n > slarg && n !== larg)
+		slarg = n;
+}
+console.log(slarg);
